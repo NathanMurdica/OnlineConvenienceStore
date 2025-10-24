15bb04718db969453fc48b384dcb642cfc08d651
Liam Cook <104545673@student.swin.edu.au>
2025-10-24 16:40:30 +1100

    Changed Item value

--- frontend/src/components/Item.vue
+++ frontend/src/components/Item.vue
@@ -2,7 +2,7 @@
     <div class="card shadow-sm h-100 text-center">
         <div class="card-body">
             <h5 class="card-title fw-semibold">Product Name</h5>
-            <p class="card-text text-muted mb-2">$00.00</p>
+            <p class="card-text text-muted mb-2">$0.00</p>
             <button class="btn btn-primary btn-sm">Add to Cart</button>
         </div>
     </div>
