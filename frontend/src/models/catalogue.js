5880774e7af8365e939e80e1a921b6845b73b9ad
Nathan Murdica <nathanmurdica@outlook.com>
2025-10-28 17:37:58 +1100

    Catalogue.vue now uses customer.js class

---
+++ frontend/src/models/catalogue.js
@@ -0,0 +1,20 @@
+// src/models/catalogue.js
+import Item from './item.js';
+import { fetchItems } from '../utils/database.js';
+
+export default class Catalogue {
+    constructor() {
+        this.items = [];
+    }
+
+    // fetch all items from backend
+    async loadItems() {
+        try {
+            const data = await fetchItems();
+            this.items = data.map(d => Item.fromJSON(d));
+        } catch (err) {
+            console.error('Failed to load catalogue items:', err);
+            throw err;
+        }
+    }
+}
