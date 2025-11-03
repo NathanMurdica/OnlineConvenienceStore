e100370d13f7dc2de70e545b144cc33dc8af521f
The Tater Lord <161768238+ReubenHolmes@users.noreply.github.com>
2025-11-03 18:21:56 +1100

    Updated CSS

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,18 +1,19 @@
 <template>
+<div class="container mt-3 card-body">
   <h1>Login</h1>
-  <div class="container card-body">
-    <form v-on:submit.prevent="login">
-      <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" v-model="email" placeholder="Email" required>
-      </div>
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required>
-      </div>
-      <button class="btn btn-primary" type="submit">Login</button>
-    </form>
-  </div>
+  
+  <form v-on:submit.prevent="login">
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="email" v-model="email" placeholder="Email" required>
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required>
+    </div>
+    <button class="btn btn-primary" type="submit">Login</button>
+  </form>
+</div>
 </template>
 
 <script setup>
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,24 +1,22 @@
 <template>
-<div class="container mt-3">
+<div class="container mt-3 card-body">
   <h1>Register</h1>
 
-  <div class="card-body">
-    <form v-on:submit.prevent="register">
-      <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="firstName" v-model="name" placeholder="Name" required>
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" v-model="email" placeholder="Email" required>
-      </div>
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required>
-      </div>
-      <button class="btn btn-primary" type="submit">Register</button>
-    </form>
-  </div>
+  <form v-on:submit.prevent="register">
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="firstName" v-model="name" placeholder="Name" required>
+    </div>
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="email" v-model="email" placeholder="Email" required>
+    </div>
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required>
+    </div>
+    <button class="btn btn-primary" type="submit">Register</button>
+  </form>
 </div>
 </template>
 
